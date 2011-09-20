340725cc0089c6e359fa2533b064259005bcc9a0
Akeem Adeniji <akeem.adeniji@gmail.com>
2011-09-20 06:53:15 -0400

    removed 'clever' implementation creating singleton methods in constructor as its not clear what is being done. created accessor for data returned from 'db.stats' call

--- lib/mongo_stats/db_stats.rb
+++ lib/mongo_stats/db_stats.rb
@@ -3,10 +3,39 @@ module MongoStats
 
     def initialize(database, connection)
       db = connection.db(database)
+      @stats = db.stats
+    end
 
-      db.stats.each{|key, value|
-        define_singleton_method(key.to_sym){value}
-      }
+    def collections
+      @stats['collections']
+    end
+
+    def objects
+      @stats['objects']
+    end
+
+    def data_size
+      @stats['dataSize']
+    end
+
+    def storage_size 
+      @stats['storageSize']
+    end
+
+    def num_extents
+      @stats['numExtents']
+    end
+
+    def indexes
+      @stats['indexes']
+    end
+
+    def index_size
+      @stats['index_size']
+    end
+
+    def ok
+      @stats['ok']
     end
 
   end
